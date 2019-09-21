06bb2e4739953e5236267b963b469810efd97736
nil <Nilrajsinh6005@ICLOUD.COM>
2019-09-21 15:47:55 +0530

    done

--- TableView-PickerView_Navigation/HomePage.swift
+++ TableView-PickerView_Navigation/HomePage.swift
@@ -15,6 +15,8 @@ class HomePage: UITableViewController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -30,7 +32,7 @@ class HomePage: UITableViewController  {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+       
         return ToDo.count
     }
 
@@ -38,14 +40,11 @@ class HomePage: UITableViewController  {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath)
         cell.textLabel?.text = ToDo[indexPath.row]
-        // Configure the cell...
+   
 
         return cell
     }
     
-
-  
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
